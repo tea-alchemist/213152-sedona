4680321dfd685e0675adc804fee2b2318d679e22
Oleg <olegggisaev@gmail.com>
2016-09-06 19:46:13 +0300

    подключаем js

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var button = document.querySelector(".search-btn");
+var popup = document.querySelector(".modal-content");
+var date = popup.querySelector("#date-1");
+var date2 = popup.querySelector("#date-2");
+var form = popup.querySelector(".search-form");
+
+button.addEventListener("click", function(event) {event.preventDefault(); popup.classList.toggle("modal-content-show");
+date.focus();
+});
+
+form.addEventListener("submit", function(event) {if (!date.value || !date2.value) {event.preventDefault();
+popup.classList.remove("modal-error");
+popup.offsetWidth = popup.offsetWidth;
+popup.classList.add("modal-error");
+}
+});
